bb5d9d5962168eaf0a8a00b0589e61efc5b53ef1
caliz83 <lizg1308@gmail.com>
2022-10-25 19:30:05 -0700

    Add code

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿bool letsPlay = true;
+
+while(letsPlay == true)
+{
+string name1;
+string time2;
+
+Console.WriteLine("What is your name?");
+name1 = Console.ReadLine();
+Console.WriteLine("What time did you wake up?");
+time2 = Console.ReadLine();
+Console.WriteLine("Your name is " + name1 + " and you woke up at " + time2 + " today.");
+
+
+bool question = true;
+while(question == true)
+{
+Console.WriteLine("Do you want to play again? Y/N");
+string playAgain = Console.ReadLine().ToUpper();
+
+if(playAgain == "Y")
+{
+    letsPlay = true;
+    question = false;
+}
+else if(playAgain == "N")
+{
+    letsPlay = false;
+    question = false;
+    Console.WriteLine("Goodbye.");
+}
+else
+{
+    Console.WriteLine("Please enter a valid option: Y or N");
+    question = true;
+}
+}
+}
